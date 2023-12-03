5c0fe6f54318bd5892149e3a428fcde718875a3f
Mehar <mehar.fatma202@gmail.com>
2023-12-03 19:54:15 +0530

    added hero section

---
+++ src/Components/Button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Button = ({ label, iconURL }) => {
+    return (
+        <button className='flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none rounded-full text-white bg-coral-red border-coral-red'>
+            {label}
+
+            <img
+                src={iconURL}
+                alt='arrow rigth icon'
+                className='ml-2 rounded-full w-5 h-5'
+            />
+        </button>
+    )
+}
+
+export default Button;
