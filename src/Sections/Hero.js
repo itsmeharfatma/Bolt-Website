e322e365270b409556f582108a2bf536a302d9ad
Mehar <mehar.fatma202@gmail.com>
2023-12-03 23:26:21 +0530

    added popular products section

--- src/Sections/Hero.js
+++ src/Sections/Hero.js
@@ -49,11 +49,11 @@ const Hero = () => {
          sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
           {shoes.map((shoe) => (
             <div key={shoe}>
-               <ShoeCard 
+              <ShoeCard
                 imgURL={shoe}
                 changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
                 bigShoeImg={bigShoeImg}
-               />
+              />
             </div>
           ))}
         </div>
