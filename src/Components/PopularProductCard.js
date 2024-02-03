337a1978b06cbc5c72954b6f6d71f947669abc77
Mehar <mehar.fatma202@gmail.com>
2024-02-03 21:42:58 +0530

    changed ui of products card

--- src/Components/PopularProductCard.js
+++ src/Components/PopularProductCard.js
@@ -11,36 +11,36 @@ const PopularProductCard = (props) => {
     const cartItemNumber = cartItems[id];
 
     return (
-        <div className='flex flex-1 flex-col w-full max-sm:w-full'>
+        <div className='flex flex-1 flex-col w-full max-md:w-56'>
             <img
                 src={imgURL}
                 alt={name}
-                className='w-[226px] h-[226px]'
+                className='w-[224px] h-[226px]'
             />
 
-            <div className='mt-3 flex items-center justify-between'>
+            <div className='mt-2 sm:mt-3 flex items-center justify-between'>
+                <h3 className='font-semibold text-xl leading-normal font-palanquin'>{name}</h3>
+
                 <div className='flex items-center gap-1'>
                     <img
                         src={star}
                         alt='rating'
-                        width={16}
-                        height={16}
+                        width={15}
+                        height={15}
                     />
                     <p className='mt-0.5 font-montserrat text-xs leading-normal text-slate-gray'>
                         (4.5)
                     </p>
                 </div>
-
-                <button onClick={() => addToCart(id)} className='flex items-center gap-1 cursor-pointer mr-0.5'>
-                    <i class="fa-solid fa-cart-shopping" style={{ color: "#FF6454" }}></i> {cartItemNumber > 0 && <> <p className='font-montserrat text-sm leading-normal text-slate-gray'>({cartItemNumber})</p> </>}
-                </button>
-
             </div>
-            <h3 className='mt-0.5 font-semibold text-xl leading-normal font-palanquin'>{name}</h3>
+
             <p className='font-semibold font-montserrat text-coral-red text-base leading-normal'>
                 <span className='text-black font-palanquin'>MRP:</span> ${price}
             </p>
-            
+
+            <button onClick={() => addToCart(id)} className='flex rounded-full justify-center bg-[#ffd426] border-2 border-[#ffc926] mt-2 py-1 items-center gap-1.5 cursor-pointer'>
+                <p className='font-semibold font-montserrat text-base leading-normal'>Add to cart</p> {cartItemNumber > 0 && <> <p className='font-montserrat text-xs leading-normal'>({cartItemNumber})</p> </>}
+            </button>
         </div>
     )
 };
